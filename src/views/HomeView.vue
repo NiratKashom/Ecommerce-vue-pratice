32e9bcc8fb352750002b74ba8ae840b39154cd47
Nirat Kashom <nirat@qboxpoint.com>
2023-02-19 13:44:13 +0700

    Navbar: setup UI

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,29 @@
 <template>
-  <hello-world />
+  <v-app>
+    <Navbar class="index" />
+    <Dashboard />
+    <Footer />
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld'
+import Dashboard from '@/components/Dashboard.vue';
+import Footer from '@/components/Footer.vue';
+import Navbar from '@/components/Navbar.vue';
 
 export default {
-  name: 'HomeView',
+  name: 'Home',
 
   components: {
-    HelloWorld,
+    Dashboard,
+    Footer,
+    Navbar
   },
 }
 </script>
+
+<style scoped>
+.index {
+  z-index: 9999
+}
+</style>
